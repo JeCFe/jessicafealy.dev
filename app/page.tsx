00d321056b20244e36295b3ce32b211bba3f31ab
Jessica Fealy <jessicaf091@outlook.com>
2024-04-11 21:14:43 +0100

    Further updates

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import Link from "next/link";
 import selfie from "@/assets/selfie3.jpg";
 import { projectData } from "@/data";
 import { TimelineItem } from "@jecfe/react-design-system/dist/cjs/components/timeline/Timeline";
+import { cva } from "class-variance-authority";
 
 const renderableContent: TimelineItem[] = projectData.map(
   (project): TimelineItem => ({
@@ -15,6 +16,7 @@ const renderableContent: TimelineItem[] = projectData.map(
         <div className="space-x-3 pb-2">
           {project.pills.map((pill, index) => (
             <Pill
+              className="bg-gradient-to-r from-pink-500  to-blue-500 bg-cover"
               size="medium"
               type="info"
               key={`${project.subheading}-${pill}-${index}`}
@@ -51,7 +53,7 @@ export default function Home() {
           <div className="flex min-h-screen w-full flex-col bg-slate-900 text-white glow:bg-slate-700/50">
             <div className="container mx-auto flex flex-col items-center justify-center pt-20 md:flex-row">
               <div className="flex w-2/3 flex-col space-y-28">
-                <div className="space-y-16 text-7xl glow:bg-gradient-to-r glow:from-pink-500 glow:via-sky-400 glow:to-blue-500 glow:bg-clip-text glow:text-transparent">
+                <div className="space-y-16 bg-gradient-to-r from-pink-500 via-sky-400 to-blue-500 bg-clip-text text-7xl text-transparent">
                   <h1>Hi there!</h1>
                   <h1 className="md:ml-40">{`I'm Jessica`} </h1>
                 </div>
@@ -60,7 +62,7 @@ export default function Home() {
                   micro-processors, and web application projects. Having build various projects including custom macro pads, secure chat server, and react design systems`}
                 </div>
 
-                <div className="flex flex-row space-x-12 md:ml-40  ">
+                <div className="flex w-full flex-row justify-center space-x-12 pb-20 md:ml-40 md:justify-start md:pb-0 ">
                   <Link
                     className="flex flex-col"
                     href="https://github.com/JeCFe"
@@ -93,7 +95,7 @@ export default function Home() {
               </div>
             </div>
             <div className="flex-grow" />
-            <div className="flex w-full flex-row items-center justify-center pb-16 pt-20 text-center md:pt-0">
+            <div className="md:visable hidden w-full flex-row items-center justify-center pb-16 pt-20 text-center md:flex md:pt-0">
               <div className="rounded-full border-2 glow:border-pink-500">
                 <Link href={""}>
                   <ArrowDown className="fill-white hover:fill-cyan-500" />
