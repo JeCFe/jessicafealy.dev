639ceebd8c1cdf138e7abfb772535b929a420b6d
Jessica Fealy <jessicaf091@outlook.com>
2024-04-19 22:50:08 +0100

    Making proficiencies open a new tab

--- components/proficiencies/Proficiencies.tsx
+++ components/proficiencies/Proficiencies.tsx
@@ -9,7 +9,7 @@ export function Proficiencies() {
       </h2>
       <div className="mt-6 flex flex-wrap gap-4">
         {proficiencies.map((x, i) => (
-          <Link href={x.href} key={`proficiencies-${i}`}>
+          <Link href={x.href} key={`proficiencies-${i}`} target="_blank">
             {x.children}
           </Link>
         ))}
