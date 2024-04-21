2fb9105ddbddfda8e18521d50c5e73965ea5af02
Jessica Fealy <jessicaf091@outlook.com>
2024-04-21 21:44:17 +0100

    Updated project content

--- assets/index.ts
+++ assets/index.ts
@@ -2,3 +2,4 @@ export * from "./logos";
 export { default as Menu } from "./menu.svg";
 export { default as OpenWeb } from "./openWeb.svg";
 export { default as Code } from "./code.svg";
+export { default as DeployedCode } from "./deployedCode.svg";
--- components/projects/Projects.tsx
+++ components/projects/Projects.tsx
@@ -1,8 +1,8 @@
-import { Anchor } from "@jecfe/react-design-system";
+import { Info } from "@jecfe/react-design-system";
 
 import { projectData } from "@/data";
 import { PageId, Pill } from "..";
-import { Code, OpenWeb } from "@/assets";
+import { Code, DeployedCode, OpenWeb } from "@/assets";
 import Link from "next/link";
 import { cva } from "class-variance-authority";
 
@@ -10,7 +10,30 @@ const links = cva(
   "inline-block h-8 w-8 fill-blue-500 transition duration-200 ease-in-out hover:fill-blue-900",
 );
 
-const customOrder = ["web", "git"]; // Desired order
+const statusText = cva(
+  "font-bold p-1 my-2 flex flex-row item-center h-full text-left",
+  {
+    variants: {
+      status: {
+        active: "text-green-500",
+        maintain: "text-amber-500",
+        closed: "text-red-500",
+      },
+    },
+  },
+);
+
+const statusIcon = cva("h-8 w-8", {
+  variants: {
+    status: {
+      active: "fill-green-500",
+      maintain: "fill-amber-500",
+      closed: "fill-red-500",
+    },
+  },
+});
+
+const customOrder = ["deployed", "web", "git"]; // Desired order
 
 function compareString(a: ProjectLink, b: ProjectLink) {
   const indexA = customOrder.indexOf(a.type);
@@ -32,16 +55,18 @@ type Props = {
   date: string;
   description: string;
   pills?: string[];
+  type?: "active" | "maintain" | "closed";
 };
 
 export function Project({ id }: { id: PageId }) {
+  const data: Props[] = projectData as Props[];
   return (
-    <div className="mt-14 flex flex-col" id={id}>
+    <div className="mt-14 flex flex-col pb-20" id={id}>
       <h2 className="w-full pb-4 text-2xl font-bold tracking-tight text-slate-200 underline decoration-1 underline-offset-8 md:text-3xl">
         Projects
       </h2>
       <div className="space-y-8 md:space-y-2">
-        {projectData.map((x, i) => (
+        {data.map((x, i) => (
           <ProjectBox {...x} key={`${x.heading}-${i}`} />
         ))}
       </div>
@@ -49,7 +74,7 @@ export function Project({ id }: { id: PageId }) {
   );
 }
 
-function ProjectBox({ heading, link, date, description, pills }: Props) {
+function ProjectBox({ heading, link, date, description, pills, type }: Props) {
   return (
     <div className="z-10 rounded-xl transition-colors duration-300 ease-in-out md:p-8 md:hover:bg-slate-800/70 md:hover:shadow-xl">
       <div className="flex flex-row">
@@ -63,6 +88,7 @@ function ProjectBox({ heading, link, date, description, pills }: Props) {
             <Link href={x.href} target="_blank">
               {x.type === "git" && <Code className={links()} />}
               {x.type === "web" && <OpenWeb className={links()} />}
+              {x.type === "deployed" && <DeployedCode className={links()} />}
             </Link>
           ))}
         </div>
@@ -71,6 +97,17 @@ function ProjectBox({ heading, link, date, description, pills }: Props) {
       <h3 className="text-sm leading-normal text-slate-400">{date}</h3>
 
       <p className="my-4 leading-normal text-slate-400">{description}</p>
+      <div className={statusText({ status: type })}>
+        <Info className={statusIcon({ status: type })} />
+        <div className="ml-4 flex items-center">
+          {type === "active" &&
+            "Actively maintained with continued feature delivery."}
+          {type === "maintain" &&
+            "Critical maintenance only with no plans for new features."}
+          {type === "closed" &&
+            "No longer maintained with no plans for new features."}
+        </div>
+      </div>
       <div className="pointer-events-none flex flex-wrap gap-2">
         {pills?.map((x, i) => <Pill key={`${x}-${i}`}>{x}</Pill>)}
       </div>
