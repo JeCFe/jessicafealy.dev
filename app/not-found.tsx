0c046c6a3a37441992bea58573892ee0c7f419a9
Jessica Fealy <jessicaf091@outlook.com>
2024-04-21 02:02:06 +0100

    not found page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,11 +1,18 @@
+import { Anchor, Info } from "@jecfe/react-design-system";
 import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <div className="flex h-screen flex-col items-center justify-center  text-slate-200">
+      <Info className="size-32 fill-red-600" />
+      <h1 className="flex text-7xl font-bold leading-tight md:text-8xl">
+        404 Not Found
+      </h1>
+
+      <p className="mt-4 text-xl">Requested resources was moved or deleted</p>
+      <Anchor href="/" className="pt-8 text-2xl">
+        Escape
+      </Anchor>
     </div>
   );
 }
